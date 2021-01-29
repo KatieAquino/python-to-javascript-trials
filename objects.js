77a312d459cf4f41a461775c5919ae53faf97178
Katie Aquino <katiemaryfrances.aquino@gmail.com>
2021-01-29 20:33:12 +0000

    Creates wordCounts and getMelonsatPrice function

--- objects.js
+++ objects.js
@@ -1,18 +1,34 @@
 "use strict";
 
 
-// 1. countWords
+// Counts amount each time word appears in given phrase
 function countWords(phrase) {
   
   const wordCounts = {};
 
-  for (word of phrase.split(' ')) {
-    if (wordCounts.hasOwnProperty(word))
+  for (const word of phrase.split(' ')) {
+    if (wordCounts.hasOwnProperty(word) == true) {
+      wordCounts[word] += 1;
+    }
+    else {
+      wordCounts[word] = 1;
+    }
   }
+  return wordCounts
 }
 
-
-// 2. getMelonsAtPrice
+// Returns a list of melons at a particular cost
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  const melonPrices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  };
+
+  if (melonPrices.hasOwnProperty(price) == false){
+    return;
+  }
+
+  return melonPrices[price].sort();
 }
