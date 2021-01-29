7767d46ee18888dad8accd0c14ac41eff33fe1f5
Katie Aquino <katiemaryfrances.aquino@gmail.com>
2021-01-29 20:44:22 +0000

    Create Function for Words in Common

--- furtherStudy.js
+++ furtherStudy.js
@@ -1,8 +1,18 @@
 "use strict";
 
-
+// returns a list of unique words that appear in words1 & words2
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+  const wordsSet1 = new Set(words1);
+  const wordsSet2 = new Set(words2);
+
+  const result = new Set();
+
+  for (const word1 of wordsSet1) {
+    if (wordsSet2.has(word1) == true) {
+      result.add(word1);
+    }
+  }
+  return result;
 }
 
 
