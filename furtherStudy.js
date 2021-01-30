e2974742c88e9b60d35a3235051c05ada70e3be4
Katie Aquino <katiemaryfrances.aquino@gmail.com>
2021-01-30 00:03:23 +0000

    Complete kidsGame

--- furtherStudy.js
+++ furtherStudy.js
@@ -17,5 +17,35 @@ function wordsInCommon(words1, words2) {
 
 
 function kidsGame(names) {
-  // Replace this with your code
+
+  //Get first words and remove from consideration, added to output
+  const output = [names.shift()];
+
+  const firstLetterToWords = {};
+
+  for (const name of names) {
+    const firstChar = name[0]
+    if (!firstLetterToWords[firstChar]) {
+      firstLetterToWords[firstChar] = name;
+    }
+    else {
+      firstLetterToWords[firstChar].push(name);
+    }
+  }
+
+  while (true) {
+
+    const lastWord = output[output.length -1];
+    const lastWordChar = lastWord[lastWord.length -1];
+
+    if (firstLetterToWords[lastWordChar] == undefined || 
+        firstLetterToWords[lastWordChar].length === 0) {
+          break;
+    }
+    else {
+      const nextWord = firstLetterToWords[lastWordChar].shift();
+      output.push(nextWord);
+    }
+  }
+  return output;
 }
